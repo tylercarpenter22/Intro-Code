503a1238309ecf8c1dd27d3b20a651f14b9a146a
Tyler Carpenter <42366218+tylercarpenter22@users.noreply.github.com>
2023-08-24 14:11:48 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+/*
+Tyler Carpenter
+
+Enter the scores for 5 subjects separated by space: 80.5 90 89.2 75.5 95
+Total score: 430.200012
+Percentage: 86.040001 %
+
+*/
+
+#include<stdio.h>
+int main(void)
+{
+    float g1, g2;
+    float total, avg;
+
+    printf("Enter the scores for 5 subjects separated by space: ");
+    scanf("%f %f", &g1, &g2);
+
+    total = g1 + g2;
+    avg = total / 2.0;
+    printf("Total score: %f/n", total);
+    printf("Percentage: %f %%", avg);
+
+}
+
+
+
+
