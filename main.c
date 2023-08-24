36cc8a84fd6282a6b674864abcaf09af6c56aadb
Tyler Carpenter <42366218+tylercarpenter22@users.noreply.github.com>
2023-08-24 15:26:43 -0400

    main.c

--- main.c
+++ main.c
@@ -10,12 +10,15 @@ Percentage: 86.040001 %
 #include<stdio.h>
 int main(void)
 {
+    //Define floats
     float g1, g2;
     float total, avg;
 
+    //get input
     printf("Enter the scores for 5 subjects separated by space: ");
     scanf("%f %f", &g1, &g2);
 
+    //Find and print the mean
     total = g1 + g2;
     avg = total / 2.0;
     printf("Total score: %f\n", total);
